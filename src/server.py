8ca15716af752d90ab79b58b28cc96410684e62f
Suraj Kesavan <spkesavan@ucdavis.edu>
2018-04-19 23:12:21 -0700

    Nodes and edges

--- src/server.py
+++ src/server.py
@@ -9,22 +9,41 @@ app.__dir__ = os.path.join(os.path.dirname(os.getcwd()), 'src')
 gf = GraphFrame()
 gf.from_hpctoolkit('../data/calc-pi')
 
+def sanitizeName(name):
+  name_split = name.split('/')
+  return name_split[len(name_split) - 1]
 
-def construct_graph_object(gf):
-  ret = {}
+
+def construct_node_object(gf):
+  ret = []
+  sankeyID = 0;
   for key, item in gf:
+    node = {}
     print key 
-    print "Inclusive Time: "
+#    print gf.get_group(key)
 #    ret['inc'] = gf[['CPUTIME (usec) (I)']].get_group(key).apply(lambda x: x.to_json(orient='records'));
-    ret['inc'] = str(gf[['CPUTIME (usec) (I)']].get_group(key).sum())
-    print gf[['CPUTIME (usec) (I)']].get_group(key).sum()
-    print gf[['CPUTIME (usec) (E)']].get_group(key).sum()
-    
-    print '\n'
+    node['inc'] = str(gf[['CPUTIME (usec) (I)']].get_group(key).sum()[0])
+    node['exc'] = str(gf[['CPUTIME (usec) (E)']].get_group(key).sum()[0])
+    node['name'] = sanitizeName(key)
+    node['sankeyID'] = sankeyID
+    sankeyID = sankeyID + 1
+    ret.append(node)
+  print ret
+  return ret
+
+def construct_edge_object(gf):
+  ret = []
+  visited, queue = set(), gf.graph.roots
+  while queue:
+    node = queue.pop(0)
+    if node.name not in ret:
+      ret.append(node.name)
+    if node not in visited:
+      visited.add(node)
+      queue.extend(node.children)
+  print ret
   return ret
   
-
-
 @app.route('/')
 def root():
   return send_from_directory(app.__dir__, 'index.html')
@@ -36,9 +55,9 @@ def send_js(filename):
 @app.route('/getSankey')
 def getSankey():
   grouped_df = gf.dataframe.groupby('module')
-  return jsonify(construct_graph_object(grouped_df))
-  
-#  return gf.graph.to_string(gf.graph.roots, gf.dataframe, threshold=0.0)
+  construct_edge_object(gf)
+  return jsonify(construct_node_object(grouped_df))
+
 
 @app.route('/dataSetInfo')
 def dataSetInfo():
