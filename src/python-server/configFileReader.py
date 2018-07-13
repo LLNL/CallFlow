f9894e5de6c442a0075e0f7ac9086d44d7e7bcb5
jarusified <jarus3001@gmail.com>
2018-07-13 12:58:37 -0700

    Better structure for the server, etc

---
+++ src/python-server/configFileReader.py
@@ -0,0 +1,49 @@
+import json
+
+class configFileReader():
+    def __init__(self, jsonFile):
+        f = open(jsonFile, 'r')
+        self.data = self.json_data(f.read())
+        self.path = self.data['paths']
+        self.props = self.data['props']
+        self.fnMap = self.getFuncMap()
+        self.fileMap = self.getFileMap()
+        return self
+
+    # File map from the config file
+    def getFileMap(self):
+        fileMap = {}
+        for obj in self.props:
+            print obj
+            name = self.props[obj].name
+            fileMap[name] = self.props[obj].files
+        return fileMap
+
+    # Function map from the config file
+    def getFuncMap(self):
+        funcMap = {}
+        print self.props
+        for obj in self.props:            
+            name = self.props[obj].name
+            funcMap[name] = self.props[obj].functions
+        return funcMap
+
+    def json_data(self, json_text):
+        return self._byteify(json.loads(json_text, object_hook=self._byteify), ignore_dicts=True)
+    
+    def _byteify(self, data, ignore_dicts = False):
+        # if this is a unicode string, return its string representation
+        if isinstance(data, unicode):
+            return data.encode('utf-8')
+        # if this is a list of values, return list of byteified values
+        if isinstance(data, list):
+            return [ self._byteify(item, ignore_dicts=True) for item in data ]
+        # if this is a dictionary, return dictionary of byteified keys and values
+        # but only if we haven't already byteified it
+        if isinstance(data, dict) and not ignore_dicts:
+            return {
+                self._byteify(key, ignore_dicts=True): self._byteify(value, ignore_dicts=True)
+                for key, value in data.iteritems()
+        }
+        # if it's anything else, return it in its original form
+        return data
--- src/python-server/server.py
+++ src/python-server/server.py
@@ -7,87 +7,87 @@ import uuid
 import argparse
 from hpctoolkit_format import *
 from caliper_format import *
+from configFileReader import * 
 
-format = ""
-gfs = []
-graphs = {}
-app = Flask(__name__, static_url_path='/public')
-app.debug = True
-app.__dir__ = os.path.join(os.path.dirname(os.getcwd()), '')
+class CallFlow():
+    def __init__(self):
+        self.app = None
+        self.args = None
+        self.cfg = None
+        self.gf = None
+        self.debug = False
+        self.config = None 
+         
+        self.create_parser()
+        self.create_server()
+        self.launch()
+         
+    def create_server(self):
+        self.app = Flask(__name__, static_url_path='/public')
+        self.app.debug = True
+        self.app.__dir__ = os.path.join(os.path.dirname(os.getcwd()), '')
+        
+        # App routes 
+        @self.app.route('/')
+        def root():
+            print "App directory", app.__dir__
+            return send_from_directory(app.__dir__, 'index.html')
+        
+        @self.app.route('/<path:filename>')
+        def send_js(filename):
+            return send_from_directory(os.path.join(app.__dir__, 'public'), filename)
+        
+        @self.app.route('/getSankey')
+        def getSankey():
+            print graphs
+            return jsonify(graphs)
+        
+        @self.app.route('/dataSetInfo')
+        def dataSetInfo():
+            return jsonify({
+                "g": 1
+            })
+                
+    def create_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--verbose", help="Display debug points")
+        parser.add_argument("--file_format", help="Format: caliper(.json) | hpctoolkit")
+        parser.add_argument("--input_file", help="Input file")
+        self.args = parser.parse_args()
 
-def parse_arguments():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--file_format", help="Format: caliper(.json) | hpctoolkit")
-  parser.add_argument("--input_file", help="Input file")
-  args = parser.parse_args()
-  return args
+        self.debug = self.args.verbose
 
-# Input: JSON File
-# Output: path to the data
-def read_file(jsonfile):
-  f = open(jsonfile, 'r')
-  json_data = json.loads(f.read())
-  path = json_data['path']
-  config = json_data['config']
-  return path
-
-# Input: paths array from JSON file
-# Output: Array of graphFrames
-def create_gf(paths, file_format):
-  ret = []
-  gf = GraphFrame()
-  if file_format == 'hpctoolkit':
-    gf.from_hpctoolkit(paths[0])
-  elif file_format == 'caliper':
-    gf.from_caliper(paths[0])
-  ret.append(gf)
-  #  for i in range(0, 1):
-  return ret
-
-def caliper_format(gfs):
-  ret = {}
-  return ret
-
-# App routes 
-@app.route('/')
-def root():
-  print "App directory", app.__dir__
-  return send_from_directory(app.__dir__, 'index.html')
-                                   
-@app.route('/<path:filename>')
-def send_js(filename):
-    return send_from_directory(os.path.join(app.__dir__, 'public'), filename)
-
-@app.route('/getSankey')
-def getSankey():
-  print graphs
-  return jsonify(graphs)
+    def launch(self):
+        # Set the format (caliper | hpctoolkit)
+        file_format = self.args.file_format
   
-@app.route('/dataSetInfo')
-def dataSetInfo():
-  return jsonify({
-    "g": 1
-    })
+        # Parse the file (--file) according to the format. 
+        self.config = configFileReader(self.args.input_file)
+  
+        # Create the graph frame 
+        gfs = self.create_gf(paths, file_format)
+
+        # Parse using the hpctoolkit format
+        if file_format == 'hpctoolkit':
+            self.cfg = hpctoolkit_format().run(gfs);
+        elif file_format == 'caliper':
+            self.cfg = caliper_callflow_format().run(gfs);
+            
+        self.app.run(debug = self.debug, use_reloader=False)
+  
+
+    # Input: paths array from JSON file
+    # Output: Array of graphFrames
+    def create_gf(self, paths, file_format):
+        ret = []
+        gf = GraphFrame()
+        if file_format == 'hpctoolkit':
+            gf.from_hpctoolkit(paths[0])
+        elif file_format == 'caliper':
+            gf.from_caliper(paths[0])
+        ret.append(gf)
+        return ret
+
 
 if __name__ == '__main__':
-  # Read arguments from command line
-  args = parse_arguments()
-  
-  # Set the format (caliper | hpctoolkit)
-  file_format = args.file_format
-  
-  # Parse the file (--file) according to the format. 
-  paths = read_file(args.input_file)
-
-  gf = GraphFrame()
-  
-  # Create the graph frame 
-  gfs = create_gf(paths, file_format)
-
-  # Parse using the hpctoolkit format
-  if file_format == 'hpctoolkit':
-    graphs = hpctoolkit_format().run(gfs);
-  elif file_format == 'caliper':
-    graphs = caliper_callflow_format().run(gfs);
-
-  app.run(debug = True, use_reloader=False)
+    CallFlow()
