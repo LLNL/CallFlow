c21a087dd5fa3bee7e03f2b8406089c9c6f7304a
jarusified <jarus3001@gmail.com>
2020-04-23 21:31:44 -0700

    amm pre-processing and adjust the colormap:

--- src/server/pipeline/create_module_map.py
+++ src/server/pipeline/create_module_map.py
@@ -4,18 +4,14 @@ from os import listdir
 from os.path import isfile, join
 
 
-fname = "counter_summary_1000.json"
-
-
-def dump_module_map(fname):
+def dump_module_map(modmap, fname):
     f = open(fname,)
     data = json.load(f)
+    # print(data)
     # -------------------------------------------------------
     # collect all functions
     functions = [n["label"] for n in data["nodes"]]
-    print(len(functions))
     # create the module map
-    modmap = {}
     for f in functions:
         toks = f.split("::")
         modname = "main" if len(toks) == 1 else toks[-2]
@@ -32,16 +28,32 @@ def dump_module_map(fname):
     # print the module map
     for k in modmap.keys():
         print("-->", k, " -->", modmap[k])
-        return modmap
+
+
+def nodeMap(fname):
+    f = open(fname,)
+    data = json.load(f)
+    functions = [n["label"] for n in data["nodes"]]
+    return functions
 
 
 def main():
     HOME = os.getcwd().split("CallFlow")[0] + "CallFlow/data"
-    directory = HOME + "/amm/"
+    directory = HOME + "/amm-24/"
     files = [f for f in listdir(directory) if isfile(join(directory, f))]
-    ret = [dump_module_map(directory + fileName) for fileName in files]
 
-    print(json.dumps(ret[0]))
+    modmap = {}
+    nodemap = {}
+    for fileName in files:
+        print(fileName)
+        dump_module_map(modmap, directory + fileName)
+
+        # nodemap[fileName] = nodeMap(directory + fileName)
+    # ret = [dump_module_map(directory + fileName) for fileName in files]
+    nodemap_file = directory + "/nodemap_amm.json"
+    print(nodemap_file)
+    with open(nodemap_file, "w") as f:
+        json.dump(nodemap, f)
 
 
 if __name__ == "__main__":
