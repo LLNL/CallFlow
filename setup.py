7691372b42a90587ac1a823fc21336c6d4c279e0
jarusified <jarus3001@gmail.com>
2020-10-14 11:18:23 -0700

    Add checks to avoid pushing all data and example content

--- setup.py
+++ setup.py
@@ -20,16 +20,47 @@ version = version["__version__"]
 
 
 # ------------------------------------------------------------------------------
+# Only allow folders in https://github.com/LLNL/CallFlow/tree/develop/data to be added.
+# For now we are hard-coding this.
+# TODO: Find a more automated solution.
+_GITHUB_DATA_FOLDERS = [
+    "caliper-cali",
+    "caliper-lulesh-json",
+    "hpctoolkit-cpi-database",
+]
+
+# Only allow jupyter notebooks in https://github.com/LLNL/CallFlow/tree/develop/example to be added.
+_GITHUB_EXAMPLE_FILES = [
+    "%callflow-ipython-magic.ipynb",
+    "CallFlow-python-interface-demo.ipynb",
+]
+
+
 # gather the data to be copied
-def list_files(directory):
+def list_files(directory, whitelist_files=[], whitelist_folders=[]):
     paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        paths.append((path, [os.path.join(path, f) for f in filenames]))
+    if len(whitelist_folders) > 0:
+        for item in os.listdir(directory):
+            if item in whitelist_folders:
+                for (path, directories, filenames) in os.walk(
+                    os.path.join(directory, item)
+                ):
+                    if ".callflow" not in path.split("/"):
+                        paths.append((path, [os.path.join(path, f) for f in filenames]))
+
+    if len(whitelist_files) > 0:
+        for (path, directories, filenames) in os.walk(directory):
+            paths.append(
+                (
+                    path,
+                    [os.path.join(path, f) for f in filenames if f in whitelist_files],
+                )
+            )
     return paths
 
 
-data_files = list_files("data")
-example_files = list_files("examples")
+data_files = list_files("data", whitelist_folders=_GITHUB_DATA_FOLDERS)
+example_files = list_files("examples", whitelist_files=_GITHUB_EXAMPLE_FILES)
 
 
 deps = [
@@ -62,7 +93,7 @@ setup(
     ],
     keywords="",
     packages=find_packages(),
-    data_files=list_files("data") + list_files("examples"),
+    data_files=data_files + example_files,
     entry_points={"console_scripts": ["callflow_server = server.callflow_server:main"]},
     install_requires=deps,
 )
