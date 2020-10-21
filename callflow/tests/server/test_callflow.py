fd6b980e8dcdbe55c862fed0b1d13d7a4accb0cc
jarusified <jarus3001@gmail.com>
2020-10-21 00:35:14 -0700

    Push some boilerplate

---
+++ callflow/tests/server/test_callflow.py
@@ -0,0 +1,13 @@
+# Copyright 2017-2020 Lawrence Livermore National Security, LLC and other
+# CallFlow Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+def test_callflow_server():
+    pass
+
+def test_socket_connection_single():
+    pass
+
+def test_socket_connection_ensemble():
+    pass
