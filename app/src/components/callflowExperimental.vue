a00f54939f2f6d678dd38cf1c2c952e9b8ffc693
suraj jarus <jarus3001@gmail.com>
2020-12-17 01:39:37 -0800

    CAL-19: Add /experimental client endpoint (#47)
    * Enable experimental route
    
    * CAL-19: Remove content for the experimental view

---
+++ app/src/components/callflowExperimental.vue
@@ -0,0 +1,65 @@
+/** 
+ * Copyright 2017-2020 Lawrence Livermore National Security, LLC and other
+ * CallFlow Project Developers. See the top-level LICENSE file for details.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+<template>
+  <v-app id="inspire">
+    <v-toolbar id="toolbar" color="teal" dark fixed app clipped-right>
+      <v-toolbar-side-icon @click.stop="left = !left">
+        <v-icon>settings</v-icon>
+      </v-toolbar-side-icon>
+      <v-toolbar-title style="margin-right: 3em">{{ appName }}</v-toolbar-title>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="left" temporary fixed>
+    </v-navigation-drawer>
+
+    <v-content class="pt-auto" v-if="selectedMode == 'Single'">
+      
+    </v-content>
+
+    <v-footer id="footer" color="teal" app>
+      Lawrence Livermore National Laboratory and VIDi Labs, University of
+      California, Davis
+      <v-spacer></v-spacer>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+import Color from "../lib/color/color";
+import Splitpanes from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
+
+import EventHandler from "./EventHandler";
+
+import * as utils from "./utils";
+
+export default {
+	name: "ExperimentalCallFlow",
+	components: {
+		
+	},
+
+	watch: {
+	
+	},
+
+	data: () => ({
+		appName: "CallFlow",		
+	}),
+
+	mounted() {
+	},
+
+	methods: {
+		
+	},
+};
+</script>
