9461767dc964a18a78b88d155a65a9e241b23c25
suraj jarus <jarus3001@gmail.com>
2020-12-17 18:03:50 -0800

    CAL-11: Componentize app vue component (#48)
    * CAL-11: Restructure App.vue
    
    * CAL-11: Fix css for the toolbar

---
+++ app/src/components/general/runtimeInformation.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-card tile>
+        <v-card-title>Runtime Information</v-card-title>
+        <v-data-table
+            dense
+            :headers="runtimeHeaders"
+            :items="data"
+            :items-per-page="5"
+            class="elevation-1"
+        >
+            <template slot="items" slot-scope="props">
+                <tr>
+                    <td nowrap="true">{{ props.item.run }}</td>
+                    <td nowrap="true">
+                        {{ props.item.min_inclusive_runtime }}
+                    </td>
+                    <td nowrap="true">
+                        {{ props.item.max_inclusive_runtime }}
+                    </td>
+                    <td nowrap="true">
+                        {{ props.item.min_exclusive_runtime }}
+                    </td>
+                    <td nowrap="true">
+                        {{ props.item.max_exclusive_runtime }}
+                    </td>
+                </tr>
+            </template>
+        </v-data-table>
+    </v-card>
+</template>
+
+<script>
+export default {
+	name: "RuntimeInformation",
+	props: ["data"],
+	data: () => ({
+		runtimeHeaders: [
+			{ text: "Run", value: "run" },
+			{
+				text: "Min. Inclusive runtime (\u03BCs)",
+				value: "min_inclusive_runtime",
+			},
+			{
+				text: "Max. Inclusive runtime (\u03BCs)",
+				value: "max_inclusive_runtime",
+				sortable: true,
+			},
+			{
+				text: "Min. Exclusive runtime (\u03BCs)",
+				value: "min_exclusive_runtime",
+			},
+			{
+				text: "Max. Exclusive runtime (\u03BCs)",
+				value: "max_exclusive_runtime",
+			},
+		],
+	}),    
+};
+</script>
