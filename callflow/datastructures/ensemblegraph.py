ebdf18d25bd9c28d174bba307250d7a742a0e354
Harsh Bhatia <bhatia4@llnl.gov>
2021-01-13 10:11:00 -0800

    changed df to dataframe

--- callflow/datastructures/ensemblegraph.py
+++ callflow/datastructures/ensemblegraph.py
@@ -42,8 +42,13 @@ class EnsembleGraph(SuperGraph):
         """
         if self.mode == "process":
 
+            self.gf = callflow.GraphFrame(dataframe=self.union_df())
+            # self.gf.dataframe = self.union_df()
+
+            '''
             self.gf = callflow.GraphFrame()
-            self.gf.df = self.union_df()
+            self.gf.dataframe = self.union_df()
+            '''
             """
             TODO: Need to write a module to convert a NetowrkX graph to a Hatchet graph.
             Currently, there is no way to convert networkX to hatchet graph yet. So we are setting this to None.
@@ -64,7 +69,7 @@ class EnsembleGraph(SuperGraph):
         df = pd.DataFrame([])
 
         for idx, tag in enumerate(self.supergraphs):
-            df = pd.concat([df, self.supergraphs[tag].gf.df], sort=True)
+            df = pd.concat([df, self.supergraphs[tag].gf.dataframe], sort=True)
         return df
 
     def union_nxg(self):
