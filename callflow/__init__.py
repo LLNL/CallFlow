3237abed7beecfb0d559425c333d6a6027890339
Harsh Bhatia <bhatia4@llnl.gov>
2021-01-13 16:36:09 -0800

    creates utils submodule and cleaned up some imports and logging

--- callflow/__init__.py
+++ callflow/__init__.py
@@ -2,20 +2,17 @@
 # CallFlow Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
+# ------------------------------------------------------------------------------
 
 # callflow.__init__.py
-from .logger import init_logger
-from logging import getLogger as get_logger
-
-#from .datastructures.graphframe import GraphFrame
-from .datastructures.supergraph import SuperGraph
-from .datastructures.ensemblegraph import EnsembleGraph
-
+from .utils.logger import init_logger, get_logger
+from .datastructures import SuperGraph, EnsembleGraph
 from .callflow import CallFlow
 
-from .server.notebook_server import _load_ipython_extension
+from .server.notebook_server import load_ipython_extension
 
-
+'''
+# ------------------------------------------------------------------------------
 def load_ipython_extension(ipython):
     """IPython API entry point.
     Only intended to be called by the IPython runtime.
@@ -23,14 +20,15 @@ def load_ipython_extension(ipython):
       https://ipython.readthedocs.io/en/stable/config/extensions/index.html
     """
     _load_ipython_extension(ipython)
-
+'''
 
 # CallFlow's public API.
 __all__ = [
     "init_logger",
     "get_logger",
-    #"GraphFrame",
     "SuperGraph",
     "EnsembleGraph",
     "CallFlow",
 ]
+
+# ------------------------------------------------------------------------------
