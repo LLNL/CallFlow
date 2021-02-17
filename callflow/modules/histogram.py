d785e9df0232466325d0a0b389b7789cb46f1af9
jarusified <jarus3001@gmail.com>
2021-02-17 14:00:33 -0800

    Add server changes from for_121_w_client branch

--- callflow/modules/histogram.py
+++ callflow/modules/histogram.py
@@ -57,15 +57,21 @@ class Histogram:
             df = self._get_data_by_histo_type(dataframe, h)[tv]
             drng = [df.min(), df.max()]
 
-            # compute the range of the relative_to_df
+            # compute the range df relative to the provided relative_to_df.
             if relative_to_df is None:
+                # Note: For single super graph, it will enter the `if` case;
                 rrng = drng
             else:
+                # Note: For ensemble super graph, we calculate relative to the
+                # ensemble_df. 
                 rdf = self._get_data_by_histo_type(relative_to_df, h)[tv]
                 rrng = [rdf.min(), rdf.max()]
-                assert rrng[0] <= drng[0]
-                assert rrng[1] >= drng[1]
-                '''
+                # TODO: Why can't relative df's min or max go beyond the bound?
+                # e.g., comparing two datasets. 
+                # I think we should rather just consider the bounds to be more
+                # flexible.
+                #assert rrng[0] <= drng[0]
+                #assert rrng[1] >= drng[1]
                 if drng[0] < rrng[0] or drng[1] > rrng[1]:
                     LOGGER.error(list(df.to_numpy()))
                     LOGGER.error(list(rdf.to_numpy()))
@@ -89,14 +95,14 @@ class Histogram:
         :param prop:
         :return:
         """
-        ndata = df_count(df, 'dataset')
+        ndatasets = df_count(df, 'dataset')
         nranks = df_count(df, 'rank')
 
         # across rank case
         if histo_type == "rank":
-            if ndata > 0:                    # ensemble case
+            if ndatasets > 0:                    # ensemble case
                 return df
-            elif ndata == 0 and nranks == 0: # single case and single rank
+            elif ndatasets == 0 and nranks == 0: # single case and single rank
                 return df
             else:                            # single case and multiple ranks
                 _df = df.groupby(["rank"])
