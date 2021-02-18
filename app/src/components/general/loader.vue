c111cb6983ba36642f852d93f1929846ae0ad170
jarusified <jarus3001@gmail.com>
2021-02-18 08:50:46 -0800

    Add loader to the data

---
+++ app/src/components/general/loader.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container v-if="!isDataReady" fill-height fluid>
+        <v-row align="center" justify="center">
+            <v-col align="center">
+                <v-progress-circular indeterminate color="teal"></v-progress-circular>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+	name: "Loader",
+	props: ["isDataReady"]
+};
+</script>
