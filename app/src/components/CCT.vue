2d839e7012c54454fa168a65ae98452930d753ba
jarusified <jarus3001@gmail.com>
2021-02-22 02:55:50 -0800

    Settings component enhanced

--- app/src/components/CCT.vue
+++ app/src/components/CCT.vue
@@ -10,24 +10,23 @@
     <Toolbar ref="Toolbar" :isSettingsOpen.sync="isSettingsOpen" />
     <v-navigation-drawer v-model.lazy="isSettingsOpen" fixed>
       <v-card fill-height>
-		<!-- TODO: Replace with Header component -->
-		<!-- <Header ref="Header" :reset="reset" :closeSettings="isSettingsOpen= !isSettingsOpen" /> -->
-		<v-flex xs12 class="ml-4 mt-4 mb-6">
-			<v-row align="center" justify="space-around">
-			<v-btn class="mx-10" icon>
-				<span> Reload </span>
-				<v-icon v-on:click="reset()">refresh</v-icon>
-			</v-btn>
-			<v-spacer></v-spacer>
-			<v-btn class="mx-4" icon>
-				<v-icon v-on:click="closeSettings()">close</v-icon>
-			</v-btn>
+        <v-col>
+			<v-row>
+				<v-icon color="teal">settings</v-icon>
+				<v-col cols="9" class="center teal--text">SETTINGS</v-col>
+				<v-btn icon>
+					<v-icon v-on:click="closeSettings()">close</v-icon>
+				</v-btn>
 			</v-row>
-		</v-flex>
-
-		<VisualEncoding ref="VisualEncoding" /> 
-
+			<v-row align="center" justify="space-around">
+				<v-btn class="mx-0" icon>
+					Reload
+					<v-icon v-on:click="reset()">refresh</v-icon>
+				</v-btn>
+			</v-row>
+		</v-col>
       </v-card>
+		<VisualEncoding ref="VisualEncoding" /> 
     </v-navigation-drawer>
 
     <v-main class="pt-0">
