61bc69c91e71a4eade81d0d2940890f07d587c53
jarusified <jarus3001@gmail.com>
2021-08-09 15:43:25 -0700

    Fix the boxplot clearing and statistics bug

--- app/src/components/boxplot/statistics.vue
+++ app/src/components/boxplot/statistics.vue
@@ -1,22 +1,22 @@
 <template>
     <v-row>
         <v-col cols="6" wrap> 
-            <v-col class="pa-0 subtitle-2">Min : {{ tData.min }}</v-col>
-            <v-col class="pa-0 subtitle-2">Max : {{ tData.max }}</v-col>
-            <v-col class="pa-0 subtitle-2">Mean : {{ tData.mean }}</v-col>
-            <v-col class="pa-0 subtitle-2">Variance : {{ tData.var }}</v-col>
-            <v-col class="pa-0 subtitle-2">Imbalance : {{ tData.imb }}</v-col>
-            <v-col class="pa-0 subtitle-2">Kurtosis : {{ tData.kurt }}</v-col>
-            <v-col class="pa-0 subtitle-2">Skewness : {{ tData.skew }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Min : {{ tData.min }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Max : {{ tData.max }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Mean : {{ tData.mean }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Variance : {{ tData.var }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Imbalance : {{ tData.imb }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Kurtosis : {{ tData.kurt }}</v-col>
+            <v-col class="pa-0 subtitle-2 green--text">Skewness : {{ tData.skew }}</v-col>
         </v-col>
         <v-col cols="6" wrap v-if="bData">  
-            <v-col class="pa-0 subtitle-2 green--text">Min : {{ bData.min }}</v-col>
-            <v-col class="pa-0 subtitle-2 green--text">Max : {{ bData.max }}</v-col>
-            <v-col class="pa-0 subtitle-2 green--text">Mean : {{ bData.mean }}</v-col>
-            <v-col class="pa-0 subtitle-2 green--text">Variance : {{ bData.var }}</v-col>
-            <v-col class="pa-0 subtitle-2 green--text">Imbalance : {{ bData.imb }}</v-col>
-            <v-col class="pa-0 subtitle-2 green--text">Kurtosis : {{ bData.kurt }}</v-col>
-            <v-col class="pa-0 subtitle-2 green--text">Skewness : {{ bData.skew }}</v-col>
+            <v-col class="pa-0 subtitle-2">Min : {{ bData.min }}</v-col>
+            <v-col class="pa-0 subtitle-2">Max : {{ bData.max }}</v-col>
+            <v-col class="pa-0 subtitle-2">Mean : {{ bData.mean }}</v-col>
+            <v-col class="pa-0 subtitle-2">Variance : {{ bData.var }}</v-col>
+            <v-col class="pa-0 subtitle-2">Imbalance : {{ bData.imb }}</v-col>
+            <v-col class="pa-0 subtitle-2">Kurtosis : {{ bData.kurt }}</v-col>
+            <v-col class="pa-0 subtitle-2">Skewness : {{ bData.skew }}</v-col>
         </v-col>
     </v-row>
 </template>
