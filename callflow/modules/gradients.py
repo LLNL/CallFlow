Fix gradients and the guides visualization

callflow/modules/gradients.py
@@ -30,37 +30,45 @@ class Gradients:
     """
 
     def __init__(
-        self, df, idx: int, ntype: str = "callsite", bins: int = 20, proxy_columns={}
+        self, sg, node, bins: int = 20, proxy_columns={}
     ):
         """
         Constructor function for the class
 
-        :param df: Dictinary of dataframes keyed by the dataset_name. For e.g., { "dataset_name": df }.
-        :param idx: Node id from the supergraph
-        :param node_type: Node type from the supergraph
+        :param sg: Dictinary of dataframes keyed by the dataset_name. For e.g., { "dataset_name": df }.
+        :param node: Super node or node
         :param bins: Number of bins to distribute the runtime information.
         :param proxy_columns: Proxy columns
         """
-        assert isinstance(df, pd.DataFrame)
-        assert ntype in ["callsite", "module"]
+        assert isinstance(sg, callflow.SuperGraph)
+        assert node.get("type") in ["callsite", "module"]
         assert isinstance(bins, int)
         assert isinstance(proxy_columns, dict)
         assert bins > 0
 
+        self.node = node
+        self.name = sg.get_name(node.get("id"), node.get("type"))
+
+        indexers = ["dataset"]
+        if node.get("type") == "callsite":
+            indexers.append("name")
+        elif node.get("type") == "module":
+            indexers.append("module")
+
+        # TODO: Could be slow for large datasets!!..
+        self.df = sg.dataframe.set_index(indexers)
+    
         # # gradient should be computed only for ensemble dataframe
         # # i.e., multiple values in dataframe column
-        self.datasets = list(df.index.levels[0])
+        self.datasets = list(self.df.index.levels[0])
         assert len(self.datasets) >= 1
 
         self.bins = bins
-        self.idx = idx
-
-        self.df = df
 
         self.proxy_columns = proxy_columns
         self.time_columns = [self.proxy_columns.get(_, _) for _ in TIME_COLUMNS]
 
-        self.max_ranks = max(df_unique(df, "rank"))
+        self.max_ranks = max(df_unique(self.df, "rank"))
         self.result = self.compute()
 
     @staticmethod
@@ -96,7 +104,6 @@ class Gradients:
         """
         # TODO: previously, this logic applied to bin edges
         # but, now, we are working on bin_centers
-        print(bins[1], bins[0])
         binw = bins[1] - bins[0]
         bin_edges = np.append(bins - 0.5 * binw, bins[-1] + 0.5 * binw)
 
@@ -131,10 +138,10 @@ class Gradients:
         for idx, dataset in enumerate(self.datasets):
             # If the level doesn't exist, it means this callsite is not present
             # in the dataset.
-            if (dataset, self.idx) not in levels:
+            if (dataset, self.node.get("id")) not in levels:
                 continue
 
-            node_df = self.df.xs((dataset, self.idx))
+            node_df = self.df.xs((dataset, self.node.get("id")))
             for tk, tv in zip(TIME_COLUMNS, self.time_columns):
                 if node_df.empty:
                     dists[tk][dataset] = dict(
@@ -158,10 +165,16 @@ class Gradients:
             # kde_grid = kde(dists_list, gridsize=num_of_bins)
 
             dataset_pos = Gradients.map_datasets_to_bins(hist_grid[0], datasets_dict)
+            pos_dataset = {bin: [] for bin in range(0, self.bins)}
+
+            for dataset in dataset_pos:
+                position = dataset_pos[dataset]
+                if dataset not in pos_dataset[position]:
+                   pos_dataset[position].append(dataset)
 
             results[tk] = {
                 "bins": num_of_bins,
-                "dataset": {"mean": dists_dict, "position": dataset_pos},
+                "dataset": {"mean": dists_dict, "d2p": dataset_pos, "p2d": pos_dataset},
                 # "kde": Histogram._format_data(kde_grid),
                 "hist": Histogram._format_data(hist_grid),
             }